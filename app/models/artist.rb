ead9f7061e5ad05e0d06af9586666a7aa7410817
PianoFF <Fangfei1star@gmail.com>
2019-11-27 13:56:40 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        songs=Song.all.select{|song| song.artist ==self}
+        songs.size
+    end 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+artists=Artist.create([
+    {
+        name: "John"
+    },
+    {
+        name: "John John"
+    },
+    {
+        name: "John John John"
+    }
+])
+
+songs_1=Song.create([
+    {
+        title: "Where are you",
+        artist_id: 3
+    },
+    {
+        title: "Wherewhere are you",
+        artist_id: 1
+    },
+    {
+        title: "Where where where are you",
+        artist_id: 3
+    }
+])
+
+songs_2=Song.create([
+    {
+        title: "Where were you",
+        artist_id: 2
+    },
+    {
+        title: "Wherewhere were you",
+        artist_id: 3
+    },
+    {
+        title: "Where where where were you",
+        artist_id: 1
+    }
+])
+
+songs_=Song.create([
+    {
+        title: "Where are you now",
+        artist_id:2
+    },
+    {
+        title: "Wherewhere are you now",
+        artist_id: 1
+    },
+    {
+        title: "Where where where are you now ",
+        artist_id: 2
+    }
+])
